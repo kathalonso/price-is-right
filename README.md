e80baa9fd99e5ad5c32d2242a7c2af85139789d2
John Carmichael <jc@stackeducation.com>
2020-01-29 17:02:33 -0500

    Initial commit of files provided to the students

---
+++ README.md
@@ -0,0 +1,4 @@
+# The Price is Right
+
+## Instructions
+In the initial commit to this project, you have been provided with files that contain a JavaScript module and some associated tests. Your task is to implement a `calculateTotal` function that will return a new list of items where each item has a new attribute called `total` with a value equal to the total price of that item. This exercise will be completed in class.
---
+++ tests.js
@@ -0,0 +1,41 @@
+const expect = require('chai').expect
+const { describe, it } = require('mocha')
+const calculateTotal = require('./index')
+
+describe('Checkout', () => {
+  it('calculates the total', () => {
+    const items = [{
+      id: 1,
+      name: 'iPhone Charger',
+      price: 19.95,
+      discount: .12,
+      quantity: 2,
+      total: 0
+    }, {
+      id: 2,
+      name: 'Video Adapter',
+      price: 12.95,
+      discount: .10,
+      quantity: 1,
+      total: 0
+    }]
+
+    const expected = [{
+      id: 1,
+      name: 'iPhone Charger',
+      price: 19.95,
+      discount: .12,
+      quantity: 2,
+      total: 35.11
+    }, {
+      id: 2,
+      name: 'Video Adapter',
+      price: 12.95,
+      discount: .10,
+      quantity: 1,
+      total: 11.65
+    }]
+
+    expect(calculateTotal(items)).to.deep.equal(expected)
+  })
+})
